e7a9eb5ac0a1c0b06a5f8f300dc553f7d70307e1
yutaka-m <alumican2000@gmail.com>
2019-10-05 15:15:57 +0900

    Update server_boot.sh

--- scripts/server_boot.sh
+++ scripts/server_boot.sh
@@ -5,7 +5,7 @@ sudo -u pi git pull origin master
 
 sudo -u pi /usr/bin/python3 /home/pi/.local/bin/ds4drv --led 00ffff --daemon
 
-/home/pi/mjpg-streamer/mjpg-streamer-experimental/mjpg_streamer -o "/home/pi/mjpg-streamer/mjpg-streamer-experimental/output_file.so -f /home/pi/mjpg-streamer/mjpg-streamer-experimental/output_files -d 67 " -o "/home/pi/mjpg-streamer/mjpg-streamer-experimental/output_http.so -w /home/pi/mjpg-streamer/mjpg-streamer-experimental/www" -i "/home/pi/mjpg-streamer/mjpg-streamer-experimental/input_raspicam.so -x 1024 -y 768 -fps 15 -q 10" -b
+/home/pi/mjpg-streamer/mjpg-streamer-experimental/mjpg_streamer -o "/home/pi/mjpg-streamer/mjpg-streamer-experimental/output_file.so -f /home/pi/mjpg-streamer/mjpg-streamer-experimental/output_files -d 67 " -o "/home/pi/mjpg-streamer/mjpg-streamer-experimental/output_http.so -w /home/pi/mjpg-streamer/mjpg-streamer-experimental/www" -i "/home/pi/mjpg-streamer/mjpg-streamer-experimental/input_raspicam.so -x 1024 -y 768 -fps 15 -q 3" -b
 
 /usr/bin/gpio mode 0 alt5
 exit 0
